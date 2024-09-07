9628f7019d9f69f668341fd80bf02a26970207ca
aaronkimbrooktec <218adk@gmail.com>
2024-09-07 12:37:49 -0400

    feat: db queries for inserting and fetching all

--- backend/internal/storage/postgres/storage.go
+++ backend/internal/storage/postgres/storage.go
@@ -1,7 +1,10 @@
 package postgres
 
 import (
+	"context"
+
 	"github.com/GenerateNU/cooked/backend/internal/settings"
+	"github.com/GenerateNU/cooked/backend/internal/types"
 	"github.com/jmoiron/sqlx"
 	_ "github.com/lib/pq"
 )
@@ -19,4 +22,19 @@ func (db *DB) Ping() error {
 	return db.db.Ping()
 }
 
-// TODO: implement the necessary queries to satisfy the storage.Storage interface
+func (db *DB) GetRecipes(ctx context.Context) ([]types.Recipe, error) {
+	var recipes []types.Recipe
+	if err := db.db.SelectContext(ctx, &recipes, "SELECT * FROM recipes"); err != nil {
+		return recipes, err
+	}
+
+	return recipes, nil
+}
+
+func (db *DB) CreateRecipe(ctx context.Context, recipe types.Recipe) (types.Recipe, error) {
+	if _, err := db.db.ExecContext(ctx, "INSERT INTO recipes (id, name, cook, instructions, image_url, meal) VALUES ($1, $2, $3, $4, $5, $6)", recipe.ID, recipe.Name, recipe.Cook, recipe.Instructions, recipe.ImageURL, recipe.Meal); err != nil {
+		return recipe, err
+	}
+
+	return recipe, nil
+}
