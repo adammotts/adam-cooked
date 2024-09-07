51fcf18122741825ad834cff70b8c630858aa238
root <root@Dao-Laptop>
2024-09-07 12:47:08 -0400

    feat: format

--- frontend/src/components/Menu/menuItem.tsx
+++ frontend/src/components/Menu/menuItem.tsx
@@ -1,27 +1,23 @@
-import Image from 'next/image'
+import Image from "next/image";
 import placeholderImg from "../../../public/placeholder.png";
 
 const MenuItem = ({ item }) => {
-    return (
-        <div className="w-[15vw] h-[25vh] bg-primary rounded-[1vw]">
-        <div className="relative w-[15vw] h-[20vh] bg-primary hover:cursor-pointer rounded-tl-[1vw] rounded-tr-[1vw]">
-            <Image
-                src={placeholderImg}
-                alt="placeholder"
-                layout="fill"
-                objectFit="cover"
-              />
-        </div>
-        <div className="flex py-[1vh] px-[0.75vw]">
-                <h1 className="flex-1">
-                    Grilled Cheese
-                </h1>
-                <h1 className="flex-2">
-                    10m
-                </h1>
-            </div>
-        </div>
-    )
-}
+  return (
+    <div className="w-[15vw] h-[25vh] bg-primary rounded-[1vw]">
+      <div className="relative w-[15vw] h-[20vh] bg-primary hover:cursor-pointer rounded-tl-[1vw] rounded-tr-[1vw]">
+        <Image
+          src={placeholderImg}
+          alt="placeholder"
+          layout="fill"
+          objectFit="cover"
+        />
+      </div>
+      <div className="flex py-[1vh] px-[0.75vw]">
+        <h1 className="flex-1">Grilled Cheese</h1>
+        <h1 className="flex-2">10m</h1>
+      </div>
+    </div>
+  );
+};
 
-export default MenuItem
+export default MenuItem;
