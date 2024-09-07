2c86bf30fcbdf0be7ebe17e0cd8ea9213f91680e
root <root@Dao-Laptop>
2024-09-07 12:14:20 -0400

    feat: frontend-progress

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,5 +1,4 @@
-
-'use client'
+"use client";
 
 import { fetchAllRecipes } from "@/api/recipe";
 import Navbar from "@/components/Navbar/Navbar";
@@ -8,30 +7,47 @@ import { Modal } from "@mui/material";
 import { useQuery } from "@tanstack/react-query";
 import { useState } from "react";
 
-export default function Home() {  
+export default function Home() {
   const [isModalOpen, setIsModalOpen] = useState(false);
 
   const { data, isLoading, error } = useQuery({
     queryKey: ["recipes"],
     queryFn: fetchAllRecipes,
-  })
+  });
+
+  const recipe = {
+    name: "grilled cheese",
+    time: "10m",
+    Instructions: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed mollis porta tristique. Aliquam a tellus at tellus blandit tristique in at ipsum. Curabitur varius velit at magna lobortis, vel efficitur sem imperdiet. Quisque ut metus id nibh ultrices hendrerit in at massa. Etiam eu sapien id mauris porttitor posuere. Nullam id tincidunt libero. Sed ex lectus, laoreet id mattis eget, bibendum sed leo. Etiam eu condimentum nunc. Donec id arcu non orci rutrum bibendum. Vestibulum a mattis lacus.",
+  }
+
+  const placeholder = [1, 2, 3, 4, 5, 6, 7];
 
   return (
     <>
       <Modal
-        className='flex flex-col items-center justify-center'
-        open={isModalOpen} 
+        className="flex flex-col items-center justify-center"
+        open={isModalOpen}
         disableAutoFocus
         onClose={() => setIsModalOpen(false)}
       >
-        <div className='bg-primary flex flex-col items-center p-16 w-[550px] h-[600px] overflow-scroll rounded-lg'>
+        <div className="bg-primary flex flex-col items-center p-16 w-[550px] h-[600px] overflow-scroll rounded-lg">
           <CreateRecipe onSuccess={() => setIsModalOpen(false)} />
         </div>
       </Modal>
       <Navbar createRecipe={() => setIsModalOpen(true)} />
-      <div className='flex flex-col p-16 items-start gap-6'>
-        { /** TODO: THIS IS WHERE THE MAIN APP LIVES */}
-      </div>
+      <div className="flex flex-col p-16 items-start gap-6">
+          <h1 className="text-[2vh]">All Recipes</h1>
+          <div className="w-full flex flex-row">
+            {placeholder.map((_, index) => (
+              <div className="flex flex-col w-full h-full overflow-x-none">
+                <div className="w-[15vw] h-[20vh] hover:cursor-pointer">
+                  hello
+                  </div>
+              </div>
+            ))}
+          </div>
+        </div>
     </>
   );
 }
